706d40d81b3fb6e97a860c3965981c34b186f002
Isaac Messer <101677325+isaac-messer@users.noreply.github.com>
2023-08-21 08:19:43 -0600

    Created Basic Components

--- Hello-World/src/app/app.module.ts
+++ Hello-World/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HerosListComponent } from './components/heros-list/heros-list.component';
+import { TopNavBarComponent } from './components/top-nav-bar/top-nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HerosListComponent,
+    TopNavBarComponent
   ],
   imports: [
     BrowserModule,
---
+++ Hello-World/src/app/components/heros-list/heros-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heros-list',
+  templateUrl: './heros-list.component.html',
+  styleUrls: ['./heros-list.component.scss']
+})
+export class HerosListComponent {
+
+}
